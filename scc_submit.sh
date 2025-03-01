f529da590612b12dfd9e60f2e6e2283d7eeec294
Qinzi Zhang <zqz20100221@126.com>
2025-03-01 17:21:14 -0500

    light modification of `train_gpt` for easier config and wandb logging

--- scc_submit.sh
+++ scc_submit.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-name="muon_single_gpu"
+# -----------------------------------------------------------------------------
+# Experiment configs
+seeds=(42 21 1009 324 5646)
+seed=${seeds[0]}
+name="muon_seed${seed}"
 
+args=(
+    "--log_folder muon"
+    "--run_name ${name}"
+    "--random_seed ${seed}"
+)
 
-BASE_DIR=/projectnb/aclab/qinziz/nanogpt-mango
+# -----------------------------------------------------------------------------
+# Redirect SCC outputs.
+BASE_DIR=/projectnb/aclab/qinziz/nanogpt-mango      # change your base path here
 DATE=$(date +"%Y-%m-%d")
-
 OUTPUT_PATH=$BASE_DIR/scc_outputs/$DATE
-
 mkdir -p $OUTPUT_PATH
 
-
+# -----------------------------------------------------------------------------
+# Submit job to SCC.
 job_output=$(qsub <<EOF
 #!/bin/bash -l
 
 #$ -pe omp 8
 #$ -l gpus=1
-#$ -l gpu_type=L40S     # Specifies the gpu type.
-#$ -l h_rt=8:00:00      # Specifies the hard time limit for the job
+#$ -l gpu_type=L40S         # Specifies the gpu type
+#$ -l h_rt=8:00:00          # Specifies the hard time limit for the job
 #$ -N "$name".sh
-#$ -o $OUTPUT_PATH/\$JOB_NAME.o\$JOB_ID     # Escape environment variables with \$
+#$ -o $OUTPUT_PATH/\$JOB_NAME.o\$JOB_ID
 #$ -e $OUTPUT_PATH/\$JOB_NAME.e\$JOB_ID
 
 source activate_env.sh
-torchrun --standalone --nproc_per_node=1 train_single_gpu.py \
-    --gpt.flex_kernel_consumer True --train.sequence_length 32768 --train.batch_size 32
+torchrun --standalone --nproc_per_node=1 train.py ${args[@]}
 EOF
 )
 
+# -----------------------------------------------------------------------------
 # Save job id and associated name to local .txt
 # This is extremely helpful to manage a bunch of experiments.
 job_id=$(echo "$job_output" | awk '{print $3}')
