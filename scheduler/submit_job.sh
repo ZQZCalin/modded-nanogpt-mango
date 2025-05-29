0f5c3dc83ba44a644b768decd9b3f3658adbdd60
Qinzi Zhang <zqz20100221@126.com>
2025-05-29 15:40:13 -0400

    modified the main training script

---
+++ scheduler/submit_job.sh
@@ -0,0 +1,114 @@
+# Contains the submit_job() function.
+
+SCHEDULE="piecewise_linear"
+
+submit_job() {
+    local lr1="$1"
+    local lr2="$2"
+    local seg="$3"                   # index-0 segment number
+    local resubmit="$4"
+
+    local seg_next=$((seg + 1))
+    local seg_prev=$((seg - 1))
+    local seg_print=$((seg + 1))    # convert to index-1 numbering
+
+    local start_step=${SEGMENTS[$seg]}
+    local end_step=${SEGMENTS[$seg_next]}
+
+    local job_name="seg${seg_print}_lr2_$(printf "%.2e" "$lr2")"
+    local output_path="${SCC_OUTPUT_PATH}/seg${seg_print}"
+    mkdir -p "$output_path"
+
+    # Checkpoint variables
+    local save_path="${CHECKPOINT_PATH}/${start_step}-${end_step}/lr2:$(printf "%.2e" "$lr2")"
+    if (( seg > 0 )); then
+        start_step_prev=${SEGMENTS[$seg_prev]}
+        load=True
+        load_path="${CHECKPOINT_PATH}/${start_step_prev}-${start_step}/lr2:$(printf "%.2e" "$lr1")"
+        load_file="iter_${start_step}.ckpt"
+    else
+        load=False
+        load_path=""
+        load_file=""
+    fi
+
+    # Clear save path (resubmit only)
+    if [[ $resubmit = true && -d $save_path ]]; then
+        rm -rf $save_path
+    fi
+
+    # Generate random sleep time to prevent race conditions
+    local rand_int=$(((RANDOM % 6000)))
+    local sleep_time=$(echo "scale=3; ($rand_int/300)" | bc)
+
+    # Submit training script
+    qsub <<EOF
+#!/bin/bash -l
+
+#$ -pe omp 8
+#$ -l h="!scc-506"          # Blacklists bad nodes. Hopefully this should fix the infinite relaunching issue.
+#$ -l gpus=1
+#$ -l gpu_type=$GPU_TYPE
+#$ -l h_rt=$GPU_HOUR
+#$ -N "$job_name"
+#$ -o $output_path/\$JOB_NAME.o\$JOB_ID
+#$ -e $output_path/\$JOB_NAME.e\$JOB_ID
+
+cd $BASE_PATH
+
+echo "\$(date '+%Y-%m-%d %H:%M:%S') - Running job \$JOB_ID..."
+
+# Manual sleep to prevent simultaneous LoadIt reads
+echo "\$(date '+%Y-%m-%d %H:%M:%S') - Sleeping ${sleep_time} seconds..."
+sleep "${sleep_time}"
+
+# Activate environment and run training
+echo "\$(date '+%Y-%m-%d %H:%M:%S') - Start training..."
+source activate_env.sh
+python main.py \
+    logging.wandb_project=$PROJECT \
+    logging.wandb_name=\$JOB_NAME \
+    logging.wandb_runid=\$JOB_ID \
+    logging.wandb_expname=$NAME \
+    logging.log_callback_data=$LOG_CALLBACK_DATA \
+    train.max_steps=$TOTAL_STEPS \
+    train.use_amp=$USE_AMP \
+    dataset.total_batch_size=$BATCH_SIZE \
+    random_seed=$RANDOM_SEED \
+    optimizer=$OPTIMIZER \
+    optimizer.beta1=$BETA1 \
+    optimizer.beta2=$BETA2 \
+    optimizer.weight_decay=$WEIGHT_DECAY \
+    optimizer.use_nesterov=$NESTEROV \
+    optimizer/lr_config=$SCHEDULE \
+    optimizer.lr_config.lr1=$lr1 \
+    optimizer.lr_config.lr2=$lr2 \
+    optimizer.lr_config.start_steps=$start_step \
+    optimizer.lr_config.max_steps=$((end_step-start_step)) \
+    checkpoint.save=True \
+    checkpoint.save_path=$save_path \
+    checkpoint.save_steps=[$end_step] \
+    checkpoint.num_steps=$((end_step-start_step)) \
+    checkpoint.load=$load \
+    checkpoint.load_path=$load_path \
+    checkpoint.load_file=$load_file \
+    checkpoint.overwrite_config=True
+
+status=\$?
+
+echo "\$(date '+%Y-%m-%d %H:%M:%S') - Job \$JOB_ID completed with status \$status"
+
+# Write progress log to progress file (with writing lock)
+log="\$(date '+%Y-%m-%d %H:%M:%S') \$JOB_ID \$status $lr1 $lr2 $seg"
+printf '%s\n' "\$log" >> $PROGRESS_PATH
+# Changed from flock and lock to printf for atomic one line writing
+# (
+#     flock -x -w 60 200 || { echo "\$(date '+%Y-%m-%d %H:%M:%S') - write error: could not write within 60 seconds" >&2; exit 1; }
+#     echo \$log >> $PROGRESS_PATH
+# ) 200>${PROGRESS_PATH}.lock
+
+echo "\$(date '+%Y-%m-%d %H:%M:%S') - Successfully updated log to the progress file."
+EOF
+
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Launcher: Submitted job with lr1=${lr1} lr2=${lr2}."
+}
